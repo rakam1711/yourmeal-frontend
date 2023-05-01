24bbcf03e2c14988f6c08c877ceee99bff5cfd2d
TheFlippantFox <theflippantfox@gmail.com>
2023-05-01 07:53:30 +0000

    configuring prettier and eslint

--- src/routes/resturant/menu/+page.server.ts
+++ src/routes/resturant/menu/+page.server.ts
@@ -3,26 +3,26 @@ import { env } from '$env/dynamic/private'
 import axios from 'axios'
 
 export const actions: Actions = {
-    default: async ({ request, cookies }) => {
-        const data = Object.fromEntries([...(await request.formData())])
-        // const { name, description, thumbnail, price, quantity, tags } = data
+	default: async ({ request, cookies }) => {
+		const data = Object.fromEntries([...(await request.formData())])
+		// const { name, description, thumbnail, price, quantity, tags } = data
 
-        const token = cookies.get('token')
-        console.log(token, env.BACKEND_SERVER_URL)
+		const token = cookies.get('token')
+		console.log(token, env.BACKEND_SERVER_URL)
 
-        const res = await axios({
-            url: `${env.BACKEND_SERVER_URL}resturant/menu/add`,
-            method: 'post',
-            headers: {
-                Authorization: `Bearer ${token}`
-            },
-            data
-        })
+		const res = await axios({
+			url: `${env.BACKEND_SERVER_URL}resturant/menu/add`,
+			method: 'post',
+			headers: {
+				Authorization: `Bearer ${token}`
+			},
+			data
+		})
 
-        console.log(res)
+		console.log(res)
 
-        return {
-            ...res.data
-        }
-    }
+		return {
+			...res.data
+		}
+	}
 }
