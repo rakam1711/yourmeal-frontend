98ce5e751f36c12ab94b53e887d7c048ae777af6
rakam <rakamsingh1711@gmail.com>
2023-05-01 20:30:07 +0530

    regular

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,22 +3,26 @@ import type { Cookies } from '@sveltejs/kit'
 import axios, { type AxiosResponse } from 'axios'
 
 interface HandleRequestOptions {
-  path: string
-  data: any
-  cookies: Cookies
+	path: string
+	data?: any
+	cookies?: Cookies
 }
 
-export const handleRequest = ({ path, data, cookies }: HandleRequestOptions): Promise<AxiosResponse> => {
-  const token = cookies.get('token')
+export const handleRequest = ({
+	path,
+	data,
+	cookies
+}: HandleRequestOptions): Promise<AxiosResponse> => {
+	const token = cookies.get('token')
 
-  console.log(BACKEND_SERVER_URL + path)
+	console.log(BACKEND_SERVER_URL + path)
 
-  return axios({
-    url: BACKEND_SERVER_URL + path,
-    method: 'post',
-    headers: {
-      Authorization: `Bearer ${token}`
-    },
-    data
-  })
+	return axios({
+		url: BACKEND_SERVER_URL + path,
+		method: 'post',
+		headers: {
+			Authorization: `Bearer ${token}`
+		},
+		data
+	})
 }
