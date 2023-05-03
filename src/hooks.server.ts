2c4eb718a60b817d1a819ac7d83cd59a3bacfbf8
TheFlippantFox <theflippantfox@gmail.com>
2023-05-03 21:13:11 +0000

    adding comments

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,14 +3,17 @@ import { redirect, type Handle } from "@sveltejs/kit"
 import axios from 'axios'
 
 export const handle: Handle = async ({ event, resolve }) => {
-  console.log(PUBLIC_BACKEND_SERVER_URL)
+  // * Protect Resturant Routes
   if (event.url.pathname.startsWith('/resturant')) {
+    // * Getting token from cookies
     const resturantToken = event.cookies.get('token')
 
+    // ? Check if someone is trying to access without a token
     if (!event.url.pathname.startsWith('/resturant/auth') && !resturantToken) {
       throw redirect(303, '/resturant/auth/login')
     }
 
+    // * Validate the token and get associated data
     if (resturantToken) {
       const response = await axios({
         url: `${PUBLIC_BACKEND_SERVER_URL}resturant/validate`,
@@ -20,18 +23,24 @@ export const handle: Handle = async ({ event, resolve }) => {
         }
       })
 
+      // * Storing the data in a cookie which expires with the session
       if (response.data.success) {
         event.cookies.set('resturant', JSON.stringify(response.data.data.resturant))
       }
     }
-
-
   }
 
+  // * Protect User Routes
   if (event.url.pathname.startsWith('/user')) {
-    const userToken = event.cookies.get('user_token')
-  }
+    // * Getting token from cookies
+    const userToken = event.cookies.get('token')
 
+    // ? Check if someone is trying to access without a token
+    if (!event.url.pathname.startsWith('/user/auth') && !userToken) {
+      throw redirect(303, '/user/auth/login')
+    }
+
+  }
 
   return resolve(event)
 }
